0093caeafeee77bf4e961a671bd8bd367d6a95b9
radearsr <radea.surya4@gmail.com>
2022-12-12 21:41:30 +0700

    add getPasswordByUsername, getIdByUsername function in userRepositoryPostgres

--- src/Infrastructures/repository/_test/UserRepositoryPostgres.test.js
+++ src/Infrastructures/repository/_test/UserRepositoryPostgres.test.js
@@ -55,26 +55,66 @@ describe("UserRespositoryPostres", () => {
       const users = await UsersTableTestHelper.findUsersById("user-123");
       expect(users).toHaveLength(1);
     });
+
+    it("should return registered user correctly", async () => {
+      // Arrange
+      const registerUser = new RegisterUser({
+        username: "radea",
+        password: "iloveyou",
+        fullname: "Radea Surya R",
+      });
+      const fakeIdGenerator = () => "123";
+      const userRepositoryPostgres = new UserRespositoryPostgres(pool, fakeIdGenerator);
+
+      // Action
+      const registeredUser = await userRepositoryPostgres.addUser(registerUser);
+
+      // Assert
+      expect(registeredUser).toStrictEqual(new RegisteredUser({
+        id: "user-123",
+        username: "radea",
+        fullname: "Radea Surya R",
+      }));
+    });
   });
 
-  it("should return registered user correctly", async () => {
-    // Arrange
-    const registerUser = new RegisterUser({
-      username: "radea",
-      password: "iloveyou",
-      fullname: "Radea Surya R",
+  describe("getPasswordByUsername", () => {
+    it("should throw InvariantError when user not found", () => {
+      const userRepositoryPostgres = new UserRespositoryPostgres(pool, {});
+
+      return expect(userRepositoryPostgres.getPasswordByUsername("radea"))
+        .rejects
+        .toThrowError(InvariantError);
     });
-    const fakeIdGenerator = () => "123";
-    const userRepositoryPostgres = new UserRespositoryPostgres(pool, fakeIdGenerator);
 
-    // Action
-    const registeredUser = await userRepositoryPostgres.addUser(registerUser);
+    it("should return username password when user is found", async () => {
+      const userRepositoryPostgres = new UserRespositoryPostgres(pool, {});
+      await UsersTableTestHelper.addUser({
+        username: "radea",
+        password: "iloveyou",
+      });
 
-    // Assert
-    expect(registeredUser).toStrictEqual(new RegisteredUser({
-      id: "user-123",
-      username: "radea",
-      fullname: "Radea Surya R",
-    }));
+      const password = await userRepositoryPostgres.getPasswordByUsername("radea");
+      expect(password).toBe("iloveyou");
+    });
+  });
+
+  describe("getIdByUsername", () => {
+    it("should throw InvariantError when user not found", async () => {
+      const userRepositoryPostgres = new UserRespositoryPostgres(pool, {});
+
+      await expect(userRepositoryPostgres.getIdByUsername("radea"))
+        .rejects
+        .toThrowError(InvariantError);
+    });
+
+    it("should return user id correctly", async () => {
+      await UsersTableTestHelper.addUser({ id: "user-123", username: "radea" });
+      const userRepositoryPostgres = new UserRespositoryPostgres(pool, {});
+
+      const userId = await userRepositoryPostgres.getIdByUsername("radea");
+
+      expect(userId).toEqual("user-123");
+    });
   });
 });
