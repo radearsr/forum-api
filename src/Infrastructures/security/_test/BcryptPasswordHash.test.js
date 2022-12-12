58422b3a06396c98ca3899c551480aec343a1979
radearsr <radea.surya4@gmail.com>
2022-12-12 20:55:18 +0700

    add new comparePassword function in BcryptPasswordHash

--- src/Infrastructures/security/_test/BcryptPasswordHash.test.js
+++ src/Infrastructures/security/_test/BcryptPasswordHash.test.js
@@ -1,20 +1,40 @@
 const bcrypt = require("bcrypt");
-const BcryptPasswordHash = require("../BcryptPasswordHash");
+const AuthenticationError = require("../../../Commons/exceptions/AuthenticationError");
+const BcryptEncryptionHelper = require("../BcryptPasswordHash");
 
-describe("BcryptPasswordHash", () => {
+describe("BcryptEncryptionHelper", () => {
   describe("hash function", () => {
     it("should encrypt password correctly", async () => {
       // Arrange
       const spyHash = jest.spyOn(bcrypt, "hash");
-      const bcryptPasswordHash = new BcryptPasswordHash(bcrypt);
+      const bcryptEncryptionHelper = new BcryptEncryptionHelper(bcrypt);
 
       // Action
-      const encryptPassword = await bcryptPasswordHash.hash("plain_password");
+      const encryptPassword = await bcryptEncryptionHelper.hash("plain_password");
 
       // Assert
       expect(typeof encryptPassword).toEqual("string");
       expect(encryptPassword).not.toEqual("plain_password");
       expect(spyHash).toBeCalledWith("plain_password", 10);
+    });
+  });
+
+  describe("comparePasword", () => {
+    it("should throw AuthenticationError if password not match", async () => {
+      const bcryptEncryptionHelper = new BcryptEncryptionHelper(bcrypt);
+
+      await expect(bcryptEncryptionHelper.comparePassword("plain_password", "encrypted_password"))
+        .rejects
+        .toThrow(AuthenticationError);
+    });
+
+    it("should not return AuthenticationError if password match", async () => {
+      const bcryptEncryptionHelper = new BcryptEncryptionHelper(bcrypt);
+      const plainPassword = "iloveyou";
+      const encryptedPassword = await bcryptEncryptionHelper.hash(plainPassword);
+
+      await expect(bcryptEncryptionHelper.comparePassword(plainPassword, encryptedPassword))
+        .resolves.not.toThrow(AuthenticationError);
     });
   });
 });
